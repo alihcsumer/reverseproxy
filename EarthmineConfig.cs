847605fc7eb44dbcdf249ece888c03dc760d63fe
alithegara <ali.h.c.sumer@gmail.com>
2020-10-09 12:50:11 +0300

    first commit

---
+++ EarthmineConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReverseProxyApplication
+{
+    public class EarthmineConfig
+    {
+        public string EarthmineURL { get; set; }
+      
+
+        public override string ToString()
+        {
+            return $"{EarthmineURL}";
+        }
+    }
+}
